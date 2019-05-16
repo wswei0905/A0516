137e286b6bddffe838c02e37a4ea3e943fa12475
wswei0905 <a85090523@gmail.com>
2019-05-16 13:05:26 +0800

    新增 spinner

--- app/src/main/java/com/example/cjcu/a123/MainActivity.java
+++ app/src/main/java/com/example/cjcu/a123/MainActivity.java
@@ -4,14 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Spinner sp_fruit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
+        button = findViewById(R.id.button);
         EditText editText = findViewById(R.id.editText);
+        sp_fruit = findViewById(R.id.sp_fruit);
+
     }
 }
